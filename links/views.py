9284c88483aaa2977259d599c7a9927c0a4d6e50
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-15 14:12:21 +0300

    working on the views now

--- links/views.py
+++ links/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponseRedirect
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView
+from links.models import Link
 
 # Create your views here.
