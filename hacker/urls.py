278ef4d21e2dc88f0b4da061d9bfcda6fa23732f
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-15 14:51:37 +0300

    altered just a fwe ursl

--- hacker/urls.py
+++ hacker/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from links.views import NewSubmissionView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('new-submission/', NewSubmissionView.as_view, name='new-submission')
 ]
