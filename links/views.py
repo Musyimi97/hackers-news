696a3968d6f928c4c2abc8242675f07078767ef5
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-15 14:19:49 +0300

    function dispatch craeted

--- links/views.py
+++ links/views.py
@@ -3,6 +3,16 @@ from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponseRedirect
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView
+
 from links.models import Link
 
 # Create your views here.
+class NewSubmissionView(object):
+    model= Link
+    fields=('title','url')
+    template_name='new_submission.html'
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(NewSubmissionView,self).dipatch(*args,**kwargs)
+
+    
