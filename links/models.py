f5a9a7544f6468075460e43236feaa7c2454b2c0
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-15 13:59:57 +0300

    building my db

--- links/models.py
+++ links/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class Link(models.Model):
+    title=models.CharField(max_length=100),
+    url=models.URLField(),
+    submitted_by= models.ForeignKey(User),
+    upvotes=models.ManyToManyField(User, related_name='votes')
+    
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
